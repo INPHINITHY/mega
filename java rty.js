982d59f3ea3965be23c1a0e7c41ea134d019d8c1
INPHINITHY <dwomohaustin14@gmail.com>
2024-03-28 12:59:56 -0500

    Initial commit

---
+++ java rty.js
@@ -0,0 +1,5 @@
+<button type="button" onclick="document.getElementById('demo').innerHTML='Hello Javascript'
+">click me</button>
+ <button type="button" onclick="document.getElementById('demo').innerHTML='i need some thing to write'
+ ">Return me</button>
+
