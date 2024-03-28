77b218f38b9ccc9619875d730dab91d841a31047
INPHINITHY <dwomohaustin14@gmail.com>
2024-03-28 13:20:11 -0500

    Removed the java test files
     i was experimenting with some jave scripts

--- java rty.js
+++
@@ -1,5 +0,0 @@
-<button type="button" onclick="document.getElementById('demo').innerHTML='Hello Javascript'
-">click me</button>
- <button type="button" onclick="document.getElementById('demo').innerHTML='i need some thing to write'
- ">Return me</button>
-
